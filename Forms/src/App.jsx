5a092ad64beab845ab9949060cb4fa6120b09e24
SamMelgar <samsama.melgar@gmail.com>
2024-02-08 13:37:23 -0500

    semi completed app

---
+++ Forms/src/App.jsx
@@ -0,0 +1,11 @@
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForms";
+
+export default function app() {
+  return (
+    <> 
+    <Authenticate/>
+    <SignUpForm/>
+    </>
+  );
+}
